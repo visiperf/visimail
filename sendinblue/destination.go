8ff2996c8a730812dbec86884f0940cb57a61dd8
Romain Jeannoutot <romain.jeannoutot@gmail.com>
2020-11-10 17:07:43 +0100

    add new destination func

--- sendinblue/destination.go
+++ sendinblue/destination.go
@@ -4,10 +4,18 @@ type destination struct {
 	Email string `json:"email"`
 }
 
+func newDestination(email string) *destination {
+	if email == "" {
+		return nil
+	}
+
+	return &destination{Email: email}
+}
+
 func newDestinations(emails []string) []*destination {
 	var destinations []*destination
 	for _, email := range emails {
-		destinations = append(destinations, &destination{Email: email})
+		destinations = append(destinations, newDestination(email))
 	}
 
 	return destinations
